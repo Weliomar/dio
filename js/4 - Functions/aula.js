007279ff8dfc10aee7f73268720cd2f42dcfab4e
“weliomar” <weliomarf@gmail.com>
2023-05-04 19:07:46 -0300

    envio de teste

--- js/4 - Functions/aula.js
+++ js/4 - Functions/aula.js
@@ -18,10 +18,20 @@ escreverNome('vitor');
 escreverNome('Weliomar')
 */
 
+/*
 function incrementarJuros(valor, percentualDeJuros) {
     const incremento = (percentualDeJuros / 100) * valor;
     return valor + incremento;
 }
 
-console.log(incrementarJuros(100,10));
+console.log(incrementarJuros(100,10));*/
+
+
+
+    function calcularMedia(nota1, nota2) {
+      let media = (nota1 + nota2) / 2;
+      return media
+    }
+
+    console.log(calcularMedia(5, 5));
 
